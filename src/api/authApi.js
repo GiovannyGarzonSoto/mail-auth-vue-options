b4bbe9c72b69ed7ddf76171fd902bf15513f160a
GiovannyGarzon <giovankyandres@gmail.com>
2023-07-10 23:02:36 -0400

    updates

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const authApi = axios.create({
-    baseURL: 'https://mail-auth-api-node-ts-production.up.railway.app/api'
+    // baseURL: 'https://mail-auth-api-node-ts-production.up.railway.app/api'
+    baseURL: 'http://localhost:3665/api'
 })
 
 export default authApi
