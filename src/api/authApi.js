cab83433bb0fc63b7b22951b936776025349e9a0
Giovanky <giovankyandres@gmail.com>
2023-07-19 01:07:29 -0400

    ngrok url to local

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const authApi = axios.create({
-    baseURL: 'https://mail-auth-api-node-ts-production.up.railway.app/api/'
+    // baseURL: 'https://mail-auth-api-node-ts-production.up.railway.app/api/'
+    baseURL: 'https://faad-200-83-84-81.ngrok-free.app/api'
 })
 
 export default authApi
