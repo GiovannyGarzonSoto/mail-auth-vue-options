41e84cec03efd938d705af6e8f8e1e619669adc4
Giovanky <giovankyandres@gmail.com>
2023-07-31 15:25:21 -0400

    updats

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const authApi = axios.create({
     // baseURL: 'https://mail-auth-api-node-ts-production.up.railway.app/api/'
-    baseURL: 'https://faad-200-83-84-81.ngrok-free.app/api'
+    // baseURL: 'https://faad-200-83-84-81.ngrok-free.app/api'
+    baseURL: 'http://localhost:3665/api'
 })
 
 export default authApi
